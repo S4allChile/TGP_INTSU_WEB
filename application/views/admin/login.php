d906e86b2616c0840880e6a1ffa8d1bad1b699b6
pfrias <pfrias@localhost>
2017-02-21 01:55:42 -0300

    Agrego hoja de estilos para login

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -8,7 +8,7 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
-        <link href="<?= base_url(); ?>/pagina/css/login.css" rel="stylesheet" type="text/css"/>
+        <link href="<?= base_url(); ?>pagina/css/login.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
           <hgroup>
