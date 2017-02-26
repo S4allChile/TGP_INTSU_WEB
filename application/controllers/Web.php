f401f7e4d28061fa91d18999cb23d5e3307f6b77
pfrias <pfrias@localhost>
2017-02-26 00:43:15 -0300

    Validaciones para marca menu

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -8,9 +8,12 @@ class Web extends CI_Controller {
         $this->load->model('web_DAO');
     }
 
-
-	public function index()
-	{
+//******************************************************************************
+        /**
+         * Funcion Base carga la pagina principal de controlador
+         */
+//******************************************************************************    
+	public function index(){
 		//******************************************************************************
 		$dataHead = array(
 			'titulo' => 'Intsu SPA | Herramientas y Mauqinarias para la construccion'
@@ -43,13 +46,81 @@ class Web extends CI_Controller {
 		$this->load->view('public/web',$datosWeb);
 
 		//******************************************************************************
-		$this->load->view('public/template/footer');
+                $datoPie = array(
+                    'encabezadoPie' => TRUE
+                );
+		$this->load->view('public/template/footer',$datoPie);
 		//******************************************************************************
 		$this->load->view('public/modales/modalWeb');
 		//******************************************************************************
-		$this->load->view('public/js/webJs');
+                $datosJs = array(
+                    'marcaMenu' => 0
+                );
+		$this->load->view('public/js/webJs',$datosJs);
 		//******************************************************************************
 
 
 	}
+        
+        
+        public function categoria(){
+        //Capturo el segmento de la url para reconocer cual es la categoria que 
+        //se esta solicitando
+            $seg = $this->uri->segment(3);
+            //desencripto la variable
+            $idSubCategoria = desencriptarUrl($seg);
+            
+            //traigo los datos de la subcategoria
+            $subCategoria = $this->web_DAO->consultaSubCategoria($idSubCategoria);
+            
+            //Traigo los datos de la categoria
+            $categoria = $this->web_DAO->consultaCategoria($subCategoria->id_categoria);
+            
+            //Busco los productos segun la subcategoria
+            $productos = $this->web_DAO->productosSubCategoria($subCategoria->id_sub_categoria);
+            
+            //******************************************************************************
+		$dataHead = array(
+			'titulo' => 'Intsu SPA | Herramientas y Mauqinarias para la construccion'
+		);
+		$this->load->view('public/template/head',$dataHead);
+		//******************************************************************************
+		$this->load->view('public/template/topBar');
+		//******************************************************************************
+		$this->load->view('public/template/logo');
+		//******************************************************************************
+		$this->load->view('public/template/carro');
+		//******************************************************************************
+		$datoMenu = array(
+			'menu' => $this->web_DAO->cargaMenu(),
+                        'subMenu' => $this->web_DAO->cargaSubMenu()
+		);
+		$this->load->view('public/template/menu',$datoMenu);
+		//******************************************************************************
+		// PAGINA PRINCIPAL
+		//******************************************************************************
+        
+                
+                
+                $datosWeb = array(
+                    'nomCategoria' => $categoria->descripcion_categoria,
+                    'nomSubCategoria' => $subCategoria->nombre_sub_categoria,
+                    'productos' => $productos
+                );
+		 $this->load->view('public/categoria',$datosWeb);
+
+		//******************************************************************************
+                 $datoPie = array(
+                    'encabezadoPie' => FALSE
+                );
+		$this->load->view('public/template/footer',$datoPie);
+		//******************************************************************************
+		$this->load->view('public/modales/modalWeb');
+		//******************************************************************************
+                $datosJs = array(
+                    'marcaMenu' => $categoria->id_categoria
+                );
+		$this->load->view('public/js/webJs',$datosJs);
+		//******************************************************************************
+        }
 }
