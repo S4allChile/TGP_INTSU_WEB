12d85712493e4b97fca72ab8a0c0f49088447509
pfrias <pfrias@localhost>
2017-02-26 00:39:49 -0300

    Se crea pagina de error 404

---
+++ application/views/public/404.php
@@ -0,0 +1,12 @@
+<div class="main-container error404">
+        <div class="entry-header">
+                <h1>Ups ! La pagina que esta solicitando no existe.</h1>
+        </div>
+    <div class="image-404"><img alt="" src="<?= base_url(); ?>images/fashion/style/img404.png"></div>
+        <div class="form404-wrapper">
+                <div class="container">
+                        <h2>La página que busca no se encuentra!</h2>
+                        <a title="Volver atras" href="#" onclick="window.history.back(-1)">Volver atras</a>
+                </div>
+        </div>
+</div>
